36986421da71d90d7fb6ec949bc7057f2de03bf2
Abby Winn <abbywinn14@gmail.com>
2022-06-16 13:03:25 -0500

    Added css classes

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -3,14 +3,12 @@ import "./ExpenseItem.css";
 
 function ExpenseItem() {
     return (
-    <div>
+        <div className='expense-item'>
         <div>June 10th 2022</div>
-        <div>
-          <h2>Car Insurance</h2>
-          <div>$294.67</div>
+        <div className='expense-item__description'>
+            <h2>Car Insurance</h2>
+            <div className='expense-item__price'>$294.67</div>
         </div>
-    </div>
-
-);
+    </div>); 
 }
 export default ExpenseItem;
