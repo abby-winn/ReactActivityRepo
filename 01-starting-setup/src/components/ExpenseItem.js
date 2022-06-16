3bb5fe62652f5497ca0a33fdd4a4791027ce5559
Aman Amjad <aman.amjad@gmail.com>
2022-06-16 13:24:32 -0500

    Updated Expenseitem

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./ExpenseItem.css";
+
 function ExpenseItem(){
+    const expsenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
     return(
-    <div>
-        <div>June 10th 2022</div>
-        <div>
-          <h2>Car Insurance</h2>
-          <div>$294.67</div>
+        <div className = 'expense-item'>
+        <div>{expsenseDate.toISOString()}</div>
+        <div className = 'expense-item__description'>
+            <h2>{expenseTitle}</h2>
+            <div className = 'expense-item__price'>${expenseAmount}</div>
         </div>
     </div>
 
