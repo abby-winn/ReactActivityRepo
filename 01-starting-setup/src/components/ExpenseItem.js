a3b7c3c5df6e416472e99bdc65339aaffc3032b8
Alan Sudhan <23sudhanalan@gmail.com>
2022-06-18 00:32:00 -0500

    Simplified Code by Creating Card.js

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate"
+import Card from "./Card.js";
 
-function ExpenseItem(props){
+function ExpenseItem(props) {
+    
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
 
-    return(
-    <div className="expense-item">
-
-      <div className="expense-item__description">
-      <ExpenseDate date={props.date}></ExpenseDate>
-
-      <div className = "expense-item__description">
-        <h2>{props.title}</h2>
-        <div className = "expense-item__price">${props.amount}</div>
-        </div>
-      </div>
-    </div>
-        );
+        ); 
 }
-
 export default ExpenseItem;
