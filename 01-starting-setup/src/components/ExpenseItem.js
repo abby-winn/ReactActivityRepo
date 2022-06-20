4282d0c9f3cbaae020dbcce7f5b598eae03b44d8
Alan Sudhan <23sudhanalan@gmail.com>
2022-06-20 09:37:26 -0500

    Added Change Title Button

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate"
 import Card from "./Card.js";
 
-function ExpenseItem(props) {
-
+function ExpenseItem(props){
+    const [title, setTitle] = useState(props.title);
+    
     function clickHandler(){
-    console.log("WOw")
+    setTitle('update');
 }
     
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
                 <button onClick = {clickHandler}>Change Title</button>
             </div>
