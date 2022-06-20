14fec43d3bcda219456bf65f2be708042afe1fcb
Alan Sudhan <23sudhanalan@gmail.com>
2022-06-20 09:21:52 -0500

    Added Change Title Button

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -4,6 +4,10 @@ import ExpenseDate from "./ExpenseDate"
 import Card from "./Card.js";
 
 function ExpenseItem(props) {
+
+    function clickHandler(){
+    console.log("WOw")
+}
     
     return (
         <Card className="expense-item">
@@ -11,6 +15,7 @@ function ExpenseItem(props) {
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+                <button onClick = {clickHandler}>Change Title</button>
             </div>
         </Card>
 
