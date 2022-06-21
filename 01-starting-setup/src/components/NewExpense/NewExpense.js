a1365a5072cda4e7f8c8922633df0be4681b6215
Alan Sudhan <23sudhanalan@gmail.com>
2022-06-21 09:09:27 -0500

    Created a button to open the form and a cancel button

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm';
 
 function NewExpense(props){
+    const [isEditing, setIsEditing] = useState(false);
+
+     function startEditingHandler() {
+        setIsEditing(true);
+    }
+
+    function stopEditingHandler() {
+        setIsEditing(false);
+    }
+
+    
+    return (
+    <div className="new-expense">
+      {isEditing===false && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {isEditing===true && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}></ExpenseForm>}
+    </div>
+  );
+
     function saveExpenseDataHandler(enteredExpenseData){
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     }
-
-    return(
-        <div className = 'new-expense'>
-            <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler}></ExpenseForm>
-        </div>
-        );
 }
 
 export default NewExpense;
