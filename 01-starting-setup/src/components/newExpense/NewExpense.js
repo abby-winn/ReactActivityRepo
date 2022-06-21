d77bd516bf9056c659047e074551286b1029c8f9
Aman Amjad <aman.amjad@gmail.com>
2022-06-21 09:11:49 -0500

    Start and cancel add new expense

--- 01-starting-setup/src/components/newExpense/NewExpense.js
+++ 01-starting-setup/src/components/newExpense/NewExpense.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm.js';
 
 function NewExpense(props){
-
+    const [isEditing, setIsEditing] = useState(false);
+    function startEditingHandler(){
+        setIsEditing(true);
+    }
+    function stopEditingHandler() {
+        setIsEditing(false);
+      }
+    
     function saveExpenseDataHandler(enteredExpenseData){
         const expenseData = {
             ...enteredExpenseData,
@@ -11,11 +18,12 @@ function NewExpense(props){
         };
 
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     }
     return(
         <div className = 'new-expense'>
-            <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler}></ExpenseForm>
-
+            {isEditing===false && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing===true && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}onCancel={stopEditingHandler}></ExpenseForm>}
         </div>
     );
 }
